c9353ebff4b687610d39ce7df375dc556207c70a
aprosikhin <aprosikhin@gmail.com>
2023-06-06 17:34:30 -0400

    - Full cleanup.

--- decision_trees.py
+++ decision_trees.py
@@ -12,30 +12,47 @@ from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
 
 
-FILE_NAME = 'GermanCredit.csv'
-LABEL_NAME = 'Class'
-LABEL_MAPPING = {'Good': 1, 'Bad': 0}
+FILE_NAME = "GermanCredit.csv"
+LABEL_NAME = "Class"
+LABEL_MAPPING = {"Good": 1, "Bad": 0}
 # Seed for pseudo-random splitting of data into test and training sets:
 DATA_SPLIT_SEED = 20
 DATA_SPLIT_TEST_FRACTION = 0.3
 
+
 # Decision Tree Params:
 DECISION_TREE_PARAM_GRID = {
-    'max_depth': [3, 4, 5],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 3],
+    "max_depth": [3, 4, 5, 6, 7, 8],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 3, 5],
 }
+NUM_CROSS_VALIDATION_FOLDS_DT = 5
 
-DEBUG_OUTPUT = True
+
+# Logistic Regression Params:
+MAX_NUM_ITERATIONS_LR = 500
+
+
+# Random Forest Params:
+RANDOM_FOREST_PARAM_GRID = {
+    "n_estimators": [30, 45, 60],
+    "max_depth": [5, 8, 10],
+    "min_samples_split": [3, 5, 8],
+    "min_samples_leaf": [1, 2, 3],
+}
+NUM_CROSS_VALIDATION_FOLDS_RF = 5
+
+
+DEBUG_OUTPUT = False
 
 
 def load_data(file_name: str, describe_columns: bool = False) -> pd.DataFrame:
     df = pd.read_csv(file_name)
     if describe_columns:
         for c in df.columns:
-            print(f'Column Name: {c}')
+            print(f"Column Name: {c}")
             print(df[c].describe())
-            print('===')
+            print("===")
     return df
 
 
@@ -47,130 +64,134 @@ def generate_feature_names(debug_output: bool = False) -> List[str]:
     return feature_names
 
 
+def print_separator():
+    print("")
+    print("===")
+    print("")
+
+
 def train_decision_tree(debug_output: bool = False) -> float:
-	"""Trains decision tree to classify the data into 'Good' and 'Bad' credit classes"""
-	clf = DecisionTreeClassifier()
+    """Trains decision tree to classify the data into 'Good' and 'Bad' credit classes.
+    Outputs area under curve (AUC) on the test data"""
+    clf = DecisionTreeClassifier()
 
-	# Dataset is not fully balanced, will use AOC curve score instead of accuracy.
-	grid_search = GridSearchCV(
-	    estimator=clf, 
-	    param_grid=DECISION_TREE_PARAM_GRID, 
-	    scoring="roc_auc",
-	    cv=5,
-	)
-	grid_search.fit(X_train, y_train)
-	best_params = grid_search.best_params_
+    # Dataset is not fully balanced, will use AOC curve score instead of accuracy.
+    grid_search = GridSearchCV(
+        estimator=clf,
+        param_grid=DECISION_TREE_PARAM_GRID,
+        scoring="roc_auc",
+        cv=NUM_CROSS_VALIDATION_FOLDS_DT,
+    )
+    grid_search.fit(X_train, y_train)
+    best_params = grid_search.best_params_
 
-	if debug_output:
-	    print("Best parameters for decision trees:", best_params)
-	    print("Best score for decision tree (on training data):", grid_search.best_score_)
+    if debug_output:
+        print(f"Best parameters for decision trees: {best_params}")
+        print(
+            f"Best score for decision tree (on training data): {grid_search.best_score_}"
+        )
 
-	clf = DecisionTreeClassifier(
-	    max_depth=best_params["max_depth"],
-	    min_samples_split=best_params["min_samples_split"],
-	    min_samples_leaf=best_params["min_samples_leaf"],
-	)
-	clf = clf.fit(X_train, y_train)
+    clf = DecisionTreeClassifier(
+        max_depth=best_params["max_depth"],
+        min_samples_split=best_params["min_samples_split"],
+        min_samples_leaf=best_params["min_samples_leaf"],
+    )
+    clf = clf.fit(X_train, y_train)
 
-	y_pred = clf.predict(X_test)
-	report = classification_report(y_test, y_pred)
-	if debug_output:
-		print(report)
+    y_pred = clf.predict(X_test)
+    report = classification_report(y_test, y_pred)
+    if debug_output:
+        print(report)
 
-	auc_score_decision_tree = roc_auc_score(y_test, y_pred)
+    auc_score_decision_tree = roc_auc_score(y_test, y_pred)
 
-	# Using Graphviz to plot the tree. Open Source.gv.pdf file to see rendered tree.
-	dot_data = tree.export_graphviz(
-	    clf, 
-	    feature_names=feature_names, 
-	    filled=True, 
-	    rounded=True, 
-	    out_file=None
-	)
-	graph = graphviz.Source(dot_data)
-	graph.view()
-	graph.render()
+    # Using Graphviz to plot the tree. Open Source.gv.pdf file to see rendered tree.
+    dot_data = tree.export_graphviz(
+        clf, feature_names=feature_names, filled=True, rounded=True, out_file=None
+    )
+    graph = graphviz.Source(dot_data)
+    graph.view()
+    graph.render()
 
-	return auc_score_decision_tree
+    return auc_score_decision_tree
+
+
+def train_logistic_regression(debug_output: bool = False) -> float:
+    clf = LogisticRegression(max_iter=MAX_NUM_ITERATIONS_LR)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    report = classification_report(y_test, y_pred)
+    if debug_output:
+        print(report)
+
+    auc_score = roc_auc_score(y_test, y_pred)
+    return auc_score
+
+
+def train_random_forest(debug_output: bool = False) -> float:
+    clf = RandomForestClassifier()
+    grid_search = GridSearchCV(
+        estimator=clf,
+        param_grid=RANDOM_FOREST_PARAM_GRID,
+        scoring="roc_auc",
+        cv=NUM_CROSS_VALIDATION_FOLDS_RF,
+    )
+
+    grid_search.fit(X_train, y_train)
+    best_params = grid_search.best_params_
+
+    if debug_output:
+        print(f"Best Parameters for forest: {best_params}")
+        print(f"Best Score for forest: {grid_search.best_score_}")
+
+    clf = RandomForestClassifier(
+        max_depth=best_params["max_depth"],
+        min_samples_leaf=best_params["min_samples_leaf"],
+        min_samples_split=best_params["min_samples_split"],
+        n_estimators=best_params["n_estimators"],
+    )
+    clf = clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    report = classification_report(y_test, y_pred)
+    if debug_output:
+        print(report)
+
+    auc_score = roc_auc_score(y_test, y_pred)
+    return auc_score
 
 
 df = load_data(FILE_NAME, describe_columns=DEBUG_OUTPUT)
 feature_names = generate_feature_names(debug_output=DEBUG_OUTPUT)
 
 X = df[feature_names]
-y = df['Class'].map(LABEL_MAPPING)
+y = df["Class"].map(LABEL_MAPPING)
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=DATA_SPLIT_TEST_FRACTION, random_state=20,
+    X,
+    y,
+    test_size=DATA_SPLIT_TEST_FRACTION,
+    random_state=20,
 )
 
 if DEBUG_OUTPUT:
-    print(f'Number of training records: {X_train.shape[0]}')
-    print(f'Number of test records: {X_test.shape[0]}')
+    print(f"Number of training records: {X_train.shape[0]}")
+    print(f"Number of test records: {X_test.shape[0]}")
 
-
-print("")
-print("===")
-print("")
-
+print_separator()
 
 decision_tree_auc_score = train_decision_tree(debug_output=DEBUG_OUTPUT)
-print(f'Tree auc_score: {decision_tree_auc_score}')
-raise ValueError('')
+print(f"AUC Score Decision Tree: {decision_tree_auc_score}")
 
-clf = DecisionTreeClassifier()
+print_separator()
 
-param_grid = {
-    "max_depth": [3, 4, 5],
-    "min_samples_split": [2, 5, 10],
-    "min_samples_leaf": [1, 2, 3],
-}
+auc_score_logistic_regression = train_logistic_regression()
+print(f"AUC Score Logistic Regression: {auc_score_logistic_regression}")
 
-# Dataset is not fully balanced, will use AOC curve score instead of accuracy.
-grid_search = GridSearchCV(
-    estimator=clf, param_grid=param_grid, scoring="roc_auc", cv=5
-)
+print_separator()
 
-grid_search.fit(X_train, y_train)
-
-# print(f"Grid search results: {grid_search.cv_results_}")
-best_params = grid_search.best_params_
-
-# Print the best parameters and best score
-print("Best Parameters:", best_params)
-print("Best Score:", grid_search.best_score_)
-
-clf = DecisionTreeClassifier(
-    max_depth=best_params["max_depth"],
-    min_samples_split=best_params["min_samples_split"],
-    min_samples_leaf=best_params["min_samples_leaf"],
-)
-
-clf = clf.fit(X_train, y_train)
-
-y_pred = clf.predict(X_test)
-# print(y_pred)
-
-# # Generate the classification report
-report = classification_report(y_test, y_pred)
-
-auc_score = roc_auc_score(y_test, y_pred)
-print("AUC Score:", auc_score)
-
-# # Print the classification report
-print(report)
-
-# # print(tree.export_graphviz(clf, out_file=None))
-dot_data = tree.export_graphviz(
-    clf, feature_names=feature_names, filled=True, rounded=True, out_file=None
-)
-
-# # Create a graph from the DOT data
-graph = graphviz.Source(dot_data)
-
-# # Display the decision tree
-graph.view()
-graph.render()
-
+auc_score_random_forest = train_random_forest()
+print(f"AUC Score Random Forest: {auc_score_random_forest}")
 
 # colormap = {0: 'red', 1: 'green'}
 
@@ -187,62 +208,3 @@ graph.render()
 
 # Show the plot
 # plt.show()
-
-print("")
-print("===")
-print("")
-
-clf = LogisticRegression(max_iter=1000)
-clf.fit(X_train, y_train)
-
-y_pred = clf.predict(X_test)
-# print(y_pred)
-
-report = classification_report(y_test, y_pred)
-print(report)
-
-auc_score = roc_auc_score(y_test, y_pred)
-print("AUC Score Linear:", auc_score)
-
-print("")
-print("===")
-print("")
-
-clf = RandomForestClassifier(max_depth=5, n_estimators=15, min_samples_split=5)
-
-param_grid = {
-    "n_estimators": [30, 45, 60],
-    "max_depth": [5, 8, 10],
-    "min_samples_split": [3, 5, 8],
-    "min_samples_leaf": [1, 2, 3],
-}
-
-grid_search = GridSearchCV(
-    estimator=clf, param_grid=param_grid, scoring="roc_auc", cv=5
-)
-
-grid_search.fit(X_train, y_train)
-
-# print(f"Grid search results: {grid_search.cv_results_}")
-best_params = grid_search.best_params_
-
-# Print the best parameters and best score
-print("Best Parameters for forest:", best_params)
-print("Best Score for forest:", grid_search.best_score_)
-
-clf = RandomForestClassifier(
-    max_depth=best_params["max_depth"],
-    min_samples_leaf=best_params["min_samples_leaf"],
-    min_samples_split=best_params["min_samples_split"],
-    n_estimators=best_params["n_estimators"],
-)
-clf = clf.fit(X_train, y_train)
-
-y_pred = clf.predict(X_test)
-# print(y_pred)
-
-# # Generate the classification report
-report = classification_report(y_test, y_pred)
-
-auc_score = roc_auc_score(y_test, y_pred)
-print("AUC Score for forest:", auc_score)
