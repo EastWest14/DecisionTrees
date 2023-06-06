bbd4056082c17f466650badd948038ec86478774
aprosikhin <aprosikhin@gmail.com>
2023-06-06 16:58:08 -0400

    - Beginning cleanup and refactoring.

--- decision_trees.py
+++ decision_trees.py
@@ -1,51 +1,138 @@
+from typing import List
+
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import graphviz
 from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
 
 
-df = pd.read_csv('GermanCredit.csv')
-for c in df.columns:
-	print(c)
-	print(df[c].describe())
-	print('===')
+FILE_NAME = 'GermanCredit.csv'
+LABEL_NAME = 'Class'
+LABEL_MAPPING = {'Good': 1, 'Bad': 0}
+# Seed for pseudo-random splitting of data into test and training sets:
+DATA_SPLIT_SEED = 20
+DATA_SPLIT_TEST_FRACTION = 0.3
 
-# feature_names = ['Property.RealEstate', 'Duration']
-feature_names = ["Duration","Amount","InstallmentRatePercentage","ResidenceDuration","Age","NumberExistingCredits","NumberPeopleMaintenance","Telephone","ForeignWorker","CheckingAccountStatus.lt.0","CheckingAccountStatus.0.to.200","CheckingAccountStatus.gt.200","CheckingAccountStatus.none","CreditHistory.NoCredit.AllPaid","CreditHistory.ThisBank.AllPaid","CreditHistory.PaidDuly","CreditHistory.Delay","CreditHistory.Critical","Purpose.NewCar","Purpose.UsedCar","Purpose.Furniture.Equipment","Purpose.Radio.Television","Purpose.DomesticAppliance","Purpose.Repairs","Purpose.Education","Purpose.Vacation","Purpose.Retraining","Purpose.Business","Purpose.Other","SavingsAccountBonds.lt.100","SavingsAccountBonds.100.to.500","SavingsAccountBonds.500.to.1000","SavingsAccountBonds.gt.1000","SavingsAccountBonds.Unknown","EmploymentDuration.lt.1","EmploymentDuration.1.to.4","EmploymentDuration.4.to.7","EmploymentDuration.gt.7","EmploymentDuration.Unemployed","Personal.Male.Divorced.Seperated","Personal.Female.NotSingle","Personal.Male.Single","Personal.Male.Married.Widowed","Personal.Female.Single","OtherDebtorsGuarantors.None","OtherDebtorsGuarantors.CoApplicant","OtherDebtorsGuarantors.Guarantor","Property.RealEstate","Property.Insurance","Property.CarOther","Property.Unknown","OtherInstallmentPlans.Bank","OtherInstallmentPlans.Stores","OtherInstallmentPlans.None","Housing.Rent","Housing.Own","Housing.ForFree","Job.UnemployedUnskilled","Job.UnskilledResident","Job.SkilledEmployee","Job.Management.SelfEmp.HighlyQualified"]
-
-X = df[feature_names]
-print(X)
-
-label_mapping = {'Good': 1, 'Bad': 0}
-y = df['Class'].map(label_mapping)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
-
-# Print the shapes of the resulting arrays
-print("X_train shape:", X_train.shape)
-print("X_test shape:", X_test.shape)
-print("y_train shape:", y_train.shape)
-print("y_test shape:", y_test.shape)
-
-
-clf = DecisionTreeClassifier()
-
-param_grid = {
+# Decision Tree Params:
+DECISION_TREE_PARAM_GRID = {
     'max_depth': [3, 4, 5],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 3],
 }
 
+DEBUG_OUTPUT = True
+
+
+def load_data(file_name: str, describe_columns: bool = False) -> pd.DataFrame:
+    df = pd.read_csv(file_name)
+    if describe_columns:
+        for c in df.columns:
+            print(f'Column Name: {c}')
+            print(df[c].describe())
+            print('===')
+    return df
+
+
+def generate_feature_names(debug_output: bool = False) -> List[str]:
+    feature_names = list(df.columns)
+    feature_names.remove(LABEL_NAME)
+    if debug_output:
+        print(feature_names)
+    return feature_names
+
+
+def train_decision_tree(debug_output: bool = False) -> float:
+	"""Trains decision tree to classify the data into 'Good' and 'Bad' credit classes"""
+	clf = DecisionTreeClassifier()
+
+	# Dataset is not fully balanced, will use AOC curve score instead of accuracy.
+	grid_search = GridSearchCV(
+	    estimator=clf, 
+	    param_grid=DECISION_TREE_PARAM_GRID, 
+	    scoring="roc_auc",
+	    cv=5,
+	)
+	grid_search.fit(X_train, y_train)
+	best_params = grid_search.best_params_
+
+	if debug_output:
+	    print("Best parameters for decision trees:", best_params)
+	    print("Best score for decision tree (on training data):", grid_search.best_score_)
+
+	clf = DecisionTreeClassifier(
+	    max_depth=best_params["max_depth"],
+	    min_samples_split=best_params["min_samples_split"],
+	    min_samples_leaf=best_params["min_samples_leaf"],
+	)
+	clf = clf.fit(X_train, y_train)
+
+	y_pred = clf.predict(X_test)
+	report = classification_report(y_test, y_pred)
+	if debug_output:
+		print(report)
+
+	auc_score_decision_tree = roc_auc_score(y_test, y_pred)
+
+	# Using Graphviz to plot the tree. Open Source.gv.pdf file to see rendered tree.
+	dot_data = tree.export_graphviz(
+	    clf, 
+	    feature_names=feature_names, 
+	    filled=True, 
+	    rounded=True, 
+	    out_file=None
+	)
+	graph = graphviz.Source(dot_data)
+	graph.view()
+	graph.render()
+
+	return auc_score_decision_tree
+
+
+df = load_data(FILE_NAME, describe_columns=DEBUG_OUTPUT)
+feature_names = generate_feature_names(debug_output=DEBUG_OUTPUT)
+
+X = df[feature_names]
+y = df['Class'].map(LABEL_MAPPING)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=DATA_SPLIT_TEST_FRACTION, random_state=20,
+)
+
+if DEBUG_OUTPUT:
+    print(f'Number of training records: {X_train.shape[0]}')
+    print(f'Number of test records: {X_test.shape[0]}')
+
+
+print("")
+print("===")
+print("")
+
+
+decision_tree_auc_score = train_decision_tree(debug_output=DEBUG_OUTPUT)
+print(f'Tree auc_score: {decision_tree_auc_score}')
+raise ValueError('')
+
+clf = DecisionTreeClassifier()
+
+param_grid = {
+    "max_depth": [3, 4, 5],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 3],
+}
+
 # Dataset is not fully balanced, will use AOC curve score instead of accuracy.
-grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, scoring='roc_auc', cv=5)
+grid_search = GridSearchCV(
+    estimator=clf, param_grid=param_grid, scoring="roc_auc", cv=5
+)
 
 grid_search.fit(X_train, y_train)
 
-print(f"Grid search results: {grid_search.cv_results_}")
+# print(f"Grid search results: {grid_search.cv_results_}")
 best_params = grid_search.best_params_
 
 # Print the best parameters and best score
@@ -53,9 +140,9 @@ print("Best Parameters:", best_params)
 print("Best Score:", grid_search.best_score_)
 
 clf = DecisionTreeClassifier(
-	max_depth=best_params['max_depth'],
-    min_samples_split=best_params['min_samples_split'],
-    min_samples_leaf=best_params['min_samples_leaf'],
+    max_depth=best_params["max_depth"],
+    min_samples_split=best_params["min_samples_split"],
+    min_samples_leaf=best_params["min_samples_leaf"],
 )
 
 clf = clf.fit(X_train, y_train)
@@ -70,17 +157,19 @@ auc_score = roc_auc_score(y_test, y_pred)
 print("AUC Score:", auc_score)
 
 # # Print the classification report
-# print(report)
+print(report)
 
 # # print(tree.export_graphviz(clf, out_file=None))
-# dot_data = tree.export_graphviz(clf, feature_names=feature_names, filled=True, rounded=True, out_file=None)
+dot_data = tree.export_graphviz(
+    clf, feature_names=feature_names, filled=True, rounded=True, out_file=None
+)
 
 # # Create a graph from the DOT data
-# graph = graphviz.Source(dot_data)
+graph = graphviz.Source(dot_data)
 
 # # Display the decision tree
-# graph.view()
-# graph.render()
+graph.view()
+graph.render()
 
 
 # colormap = {0: 'red', 1: 'green'}
@@ -99,3 +188,61 @@ print("AUC Score:", auc_score)
 # Show the plot
 # plt.show()
 
+print("")
+print("===")
+print("")
+
+clf = LogisticRegression(max_iter=1000)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+# print(y_pred)
+
+report = classification_report(y_test, y_pred)
+print(report)
+
+auc_score = roc_auc_score(y_test, y_pred)
+print("AUC Score Linear:", auc_score)
+
+print("")
+print("===")
+print("")
+
+clf = RandomForestClassifier(max_depth=5, n_estimators=15, min_samples_split=5)
+
+param_grid = {
+    "n_estimators": [30, 45, 60],
+    "max_depth": [5, 8, 10],
+    "min_samples_split": [3, 5, 8],
+    "min_samples_leaf": [1, 2, 3],
+}
+
+grid_search = GridSearchCV(
+    estimator=clf, param_grid=param_grid, scoring="roc_auc", cv=5
+)
+
+grid_search.fit(X_train, y_train)
+
+# print(f"Grid search results: {grid_search.cv_results_}")
+best_params = grid_search.best_params_
+
+# Print the best parameters and best score
+print("Best Parameters for forest:", best_params)
+print("Best Score for forest:", grid_search.best_score_)
+
+clf = RandomForestClassifier(
+    max_depth=best_params["max_depth"],
+    min_samples_leaf=best_params["min_samples_leaf"],
+    min_samples_split=best_params["min_samples_split"],
+    n_estimators=best_params["n_estimators"],
+)
+clf = clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+# print(y_pred)
+
+# # Generate the classification report
+report = classification_report(y_test, y_pred)
+
+auc_score = roc_auc_score(y_test, y_pred)
+print("AUC Score for forest:", auc_score)
